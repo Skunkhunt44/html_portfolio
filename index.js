065bf61a72a3a9a45098ad0fbd272ae4d3ce533d
kwameAdams <kwameadams77@gmail.com>
2024-04-16 13:50:37 +0100

    update

--- index.js
+++ index.js
@@ -10,3 +10,25 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+function sendEmail(){
+  
+    let params = {
+        name:document.getElementById('name').value,
+        email:document.getElementById('email').value,
+        subject:document.getElementById('subject').value,
+        message:document.getElementById('message').value
+    }
+    emailjs.send("service_9h5watf","template_qtq17aw",params)
+    .then(
+        alert("Email sent")
+    )
+    .catch(error => {
+        console.error("Email send error:", error);
+        alert("Failed to send email. Please try again later.");
+    });
+
+    
+
+
+}
